8565ec3ccfbfa5cec8ff5317ccc93f4e8b45f657
Sabbir Rahman <68667934+SABBIR657@users.noreply.github.com>
2020-08-17 23:19:01 +0100

    added html css and javascript file

---
+++ script.js
@@ -0,0 +1,115 @@
+//Challenge 1: Your age in Days
+
+function ageInDays(){
+var birthYear = prompt("what year were you born...Good friend?");
+var year = prompt("what year is it?");
+var ageInDayss = (year-birthYear)*365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('you are '+ ageInDayss + ' days old');
+h1.setAttribute('id', 'ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1);
+}
+function reset()
+{
+    document.getElementById('ageInDays').remove();
+}
+
+//prgorammer
+
+function programmer()
+{
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-programmer');
+    image.src = "images/sabbir.jpg";
+    let width = image.getAttribute('width');
+    let height = image.getAttribute('height');
+    image.setAttribute('width',200);
+    image.setAttribute('height',300);
+    div.appendChild(image);
+}
+
+//challenge 3; Rock, paper,scissers
+function rpsGame(yourChoice)
+{
+    console.log(yourChoice);
+    var  humanChoice, botChoice;
+    humanChoice = yourChoice.id;
+   
+    botChoice =numberToChoice(randToRpsInt());
+     console.log('Computer choice: ',botChoice);
+   
+    result = decideWinner(humanChoice,botChoice); //[0,1] human lost ! bot won
+   console.log(result);
+  
+   message = finalMessage(result); //{message: 'you won', 'color':'green;
+   console.log(message);
+
+  rpsFrontEnd(yourChoice.id,botChoice,message);
+
+}
+
+function randToRpsInt()
+{
+    return Math.floor(Math.random()*3);
+}
+
+function numberToChoice(number)
+{
+    return['rock','paper','scissors'][number];
+}
+
+function decideWinner(yourChoice, computerChoice)
+{
+    var rpsDatabase = {
+        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
+        'paper': {'rock': 1, 'paper':0.5, 'scissors': 0}, //you may also use if else statement
+        'scissors': {'paper':1, 'scissors':0.5, 'rock':0},//like if comouter pick paper and you pick scissors you lost like this 
+    }
+    var yourScore = rpsDatabase[yourChoice][computerChoice];
+    var computerScore = rpsDatabase[computerChoice][yourChoice];
+
+    return [yourScore, computerScore];
+}
+function finalMessage([yourScore, computerScore])
+{
+    if(yourScore === 0)
+    {
+        return{'message': 'You Lost!', 'color':'red'};
+    }
+    else if(yourScore === 0.5)
+    {
+        return {'message': 'You tied','color': 'yellow'};
+    }
+    else
+    {
+        return{'message': 'You won!','color': 'green'};
+    }
+
+}
+
+function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage)
+{
+    var imagesDatabase = {
+        'rock': document.getElementById('rock').src,
+        'paper': document.getElementById('paper').src,
+        'scissors': document.getElementById('scissors').src
+    }
+    //lets remove all the images
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+  
+    var humanDiv = document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML = "<img src='" + imagesDatabase[humanImageChoice] + "'height=150 width=150 style='box-shadow: 0px 10px 50px rgba(37, 50, 233,1);'>"
+    messageDiv.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding:30px; '>" + finalMessage['message'] + "</h1>" //it;s says <h1 style='color:green;'>you won! </h1>
+    botDiv.innerHTML = "<img src='" + imagesDatabase[botImageChoice] + "'height=150 width=150 style='box-shadow: 0px 10px 50px rgba(243, 38, 24,1);'>"
+  
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+   
+}
